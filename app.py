6d8b8bcfa2c5d38d86018777a2ab72a9dc58f666
Mark Kooijman <cagedev@gmail.com>
2021-01-21 15:06:13 +0100

    added template

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+import serial
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return '<h1>Hello, World!</h1>'
+    # return '<h1>Hello, World!</h1>'
+    return render_template('schaakbord.html')
 
 
-@app.route('/index/')
-def hello_index():
-    return 'Hello, Index2!'
+@app.route('/zet/<x1>/<y1>/<x2>/<y2>')
+# def zet(x1, y1, x2, y2):
+def zet(x1, y1, x2, y2):
+    # seriele_poort = serial.Serial('/dev/ttyUSB0', 9600)
+    # seriele_poort.write(x1)
+    # seriele_poort.write(y1)
+    # seriele_poort.write(x2)
+    # seriele_poort.write(y2)
+    return "x1="+x1+"y1="+y1+"x2="+x2+"y2="+y2 + "\n Is hopelijk gelukt..."
